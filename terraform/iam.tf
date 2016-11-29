52991cf35469942ff9e7631d02264192dc5d9092
Peter Sellars <psellars@gmail.com>
2016-11-29 15:36:22 +1300

    add ecs_service_role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -12,3 +12,16 @@ resource "aws_iam_role_policy" "ecs_instance_role_policy" {
   policy   = "${file("policies/ecs-instance-role-policy.json")}"
   role     = "${aws_iam_role.ecs_instance_role.id}"
 }
+
+/* ECS Service Scheduler Role */
+resource "aws_iam_role" "ecs_service_role" {
+  name               = "ecs_service_role"
+  assume_role_policy = "${file("policies/ecs-role.json")}"
+}
+
+/* ECS Service Scheduler Role & Policy */
+resource "aws_iam_role_policy" "ecs_service_role_policy" {
+  name     = "ecs_service_role_policy"
+  policy   = "${file("policies/ecs-service-role-policy.json")}"
+  role     = "${aws_iam_role.ecs_service_role.id}"
+}
--- terraform/spec/iam_spec.rb
+++ terraform/spec/iam_spec.rb
@@ -16,3 +16,18 @@ describe iam_role('ecs_instance_role') do
   it { should be_allowed_action('logs:CreateLogStream') }
   it { should be_allowed_action('logs:PutLogEvents') }
 end
+
+describe iam_role('ecs_service_role') do
+  it { should exist }
+  it { should have_inline_policy('ecs_service_role_policy') }
+end
+
+describe iam_role('ecs_service_role') do
+  it { should be_allowed_action('ec2:AuthorizeSecurityGroupIngress') }
+  it { should be_allowed_action('ec2:Describe') }
+  it { should be_allowed_action('elasticloadbalancing:DeregisterInstancesFromLoadBalancer') }
+  it { should be_allowed_action('elasticloadbalancing:DeregisterTargets') }
+  it { should be_allowed_action('elasticloadbalancing:Describe') }
+  it { should be_allowed_action('elasticloadbalancing:RegisterInstancesWithLoadBalancer') }
+  it { should be_allowed_action('elasticloadbalancing:RegisterTargets') }
+end
